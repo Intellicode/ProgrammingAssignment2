c232d3b294df4b096f307cc52f7c1b67301b4fec
Tom Hastjarjanto <tom@intellicode.nl>
2016-02-21 16:20:31 +0100

    Add cached matrix implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module contains utility functions to speed up calculations
+## of the matrix inverse operation
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list containing helper functions 
+## to set and get its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates or returns the cached result the inverse of a cacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
